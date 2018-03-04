cec691a5954a3d6dc63d555246f2668b44436178
Markus Heiser <markus.heiser@darmarit.de>
2018-03-04 16:10:30 +0100

    winShortcuts: Fehlermeldung wenn Umgebungsvariablen nicht gesetzt
    BTW wurde noch aus Commit 93c2ae28 in das Template gemerged
    
      CDB15: CADDOK_INSTALLDIR ist in CDB-15 erforderlich

--- templates/winShortcuts/cdbEnv.bat
+++ templates/winShortcuts/cdbEnv.bat
@@ -9,7 +9,7 @@ REM ----------------------------------------------------------------------------
 REM Purpose:     general CDB setup
 REM ----------------------------------------------------------------------------
 
-color 0E
+REM color 0E
 
 REM CDB setup
 REM ---------------
@@ -18,6 +18,7 @@ SET "CADDOK_DBNAME=prod_copy"
 SET "CADDOK_RUNTIME=C:\share\cdb10_1_sl48"
 SET "CADDOK_BASE=C:\share\customer\instance_prod_copy"
 
+SET "CADDOK_INSTALLDIR=%CADDOK_RUNTIME%"
 SET "CADDOK_LOGDIR=%CADDOK_BASE%\tmp"
 SET "CADDOK_DEFAULT=%CADDOK_DBNAME%@%CADDOK_BASE%"
 SET "CADDOK_CLIENT_HOME=%CADDOK_RUNTIME%"
@@ -34,3 +35,24 @@ REM SET "CDBTOOLS_HOME=C:\share\cdb-tools"
 
 SET "CDBTOOLS_DEBUG_ADDR=127.0.0.1"
 SET "CDBTOOLS_DEBUG_PORT=4444"
+
+
+goto checkEnv
+
+:errorEnv
+echo.
+echo CADDOK_RUNTIME: %CADDOK_RUNTIME%
+echo CADDOK_BASE:    %CADDOK_BASE%
+echo CADDOK_DEFAULT: %CADDOK_DEFAULT%
+echo.
+ECHO ERROR: Die Umgebungsvariablen in der Datei::
+ECHO ERROR:   %~d0%~p0cdbEnv.bat
+ECHO ERROR: Sind (noch) nicht richtig konfiguriert!
+pause
+START notepad %~d0%~p0cdbEnv.bat
+exit
+
+:checkEnv
+
+IF NOT EXIST %CADDOK_RUNTIME% goto errorEnv
+IF NOT EXIST %CADDOK_BASE% goto errorEnv
