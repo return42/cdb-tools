6da47409ec32b96e969bc298e46ad54e21e65369
Markus Heiser <markus.heiser@darmarit.de>
2019-03-15 16:43:48 +0100

    templates: cdbEnv.bat aktualisiert

--- templates/winShortcuts/cdbEnv.bat
+++ templates/winShortcuts/cdbEnv.bat
@@ -2,7 +2,7 @@
 @echo off
 REM ----------------------------------------------------------------------------
 REM --                             --  File:     cdbEnv.bat
-REM -- Copyright (C) 2017 darmarIT --  Author:   Markus Heiser
+REM -- Copyright (C) 2019 darmarIT --  Author:   Markus Heiser
 REM --     All rights reserved     --  mail:     markus.heiser@darmarIT.de
 REM --                             --  http://www.darmarIT.de
 REM ----------------------------------------------------------------------------
@@ -11,17 +11,21 @@ REM ----------------------------------------------------------------------------
 
 REM color 0E
 
+REM LC_ALL is used by ConEmu
+SET LC_ALL=de_DE.UTF-8
+
 REM CDB setup
 REM ---------------
 
-SET "CADDOK_DBNAME=prod_copy"
-SET "CADDOK_RUNTIME=C:\share\cdb10_1_sl48"
-SET "CADDOK_BASE=C:\share\customer\instance_prod_copy"
+SET "CADDOK_BASE=C:\share\cdb_cust_dev"
+SET "CADDOK_DBNAME=cust_dev"
+
+SET "CADDOK_RUNTIME=C:\share\contact\cdbsrv-11.3.10"
+SET "CADDOK_CLIENT_HOME=C:\share\contact\cdbpc-11.3.0.10"
 
 SET "CADDOK_INSTALLDIR=%CADDOK_RUNTIME%"
 SET "CADDOK_LOGDIR=%CADDOK_BASE%\tmp"
 SET "CADDOK_DEFAULT=%CADDOK_DBNAME%@%CADDOK_BASE%"
-SET "CADDOK_CLIENT_HOME=%CADDOK_RUNTIME%"
 
 REM CDB-Tools setup
 REM ---------------
@@ -36,6 +40,9 @@ REM SET "CDBTOOLS_HOME=C:\share\cdb-tools"
 SET "CDBTOOLS_DEBUG_ADDR=127.0.0.1"
 SET "CDBTOOLS_DEBUG_PORT=4444"
 
+SET PIP_PY_PLATFORM=win_amd64
+REM CDB ELEMENTS prior 15.3 need 'win32'
+REM SET PIP_PY_PLATFORM=win32
 
 goto checkEnv
 
@@ -47,12 +54,13 @@ echo CADDOK_DEFAULT: %CADDOK_DEFAULT%
 echo.
 ECHO ERROR: Die Umgebungsvariablen in der Datei::
 ECHO ERROR:   %~d0%~p0cdbEnv.bat
-ECHO ERROR: Sind (noch) nicht richtig konfiguriert!
+ECHO ERROR: Sind noch nicht richtig konfiguriert!
 pause
 START notepad %~d0%~p0cdbEnv.bat
 exit
 
 :checkEnv
-
-IF NOT EXIST %CADDOK_RUNTIME% goto errorEnv
-IF NOT EXIST %CADDOK_BASE% goto errorEnv
+IF NOT [%_CHECK_ENV%]==[N] (
+  IF NOT EXIST %CADDOK_RUNTIME% goto errorEnv
+  IF NOT EXIST %CADDOK_BASE% goto errorEnv
+)
